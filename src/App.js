8950b2a1e628dece391f13f6c7613d7b60b69948
ArchILLtect <nickhansonsr@gmail.com>
2025-05-10 21:52:06 -0500

    Remove OG code for App.js and sdd temp code for project 4; Update App.css to correspond with changes;

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-        <header className="App-header">
+            <img class="mainImage" src="images/bullpen_practice.jpg" alt="Bullpen"/>
             <h1>Welcome to The Bullpen Report</h1>
-            <p>Created by Nick Hanson - Built with React</p>
-            <p>Version 0.1.0 - Using the Bullpen API</p>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-        </header>
-
-
+            <h2>Created by Nick Hanson - Built with React</h2>
+            <h3>Version 0.1.0 - Using the Bullpen API</h3>
         </div>
     );
 }
