7c73a8c66364fb2c5f4563d9781705e1d489c583
ArchILLtect <nickhansonsr@gmail.com>
2025-05-17 13:50:18 -0500

    add: React Router; add routes to add.js; add Navbar to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+/*
+Created by Nick Hanson
+Last modified: 5/17/25
+
+This file is part of The Bullpen Report project.
+The Bullpen Report is a baseball statistics viewer powered by AWS services.
+It connects to a custom API built using Lambda, API Gateway, and DynamoDB,
+delivering live team, player, and game data directly to the browser.
+The Bullpen Report is licensed under the MIT License.
+See the LICENSE file in the project root for more information.
+*/
+
 import './App.css';
+import Navbar from './Components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './About';
+import { Contact } from './Contact';
+import { Stats } from './Stats';
 
-function App() {
-    return (
-        <div className="App">
-            <img class="mainImage" src="images/bullpen_practice.jpg" alt="Bullpen"/>
-            <h1>Welcome to The Bullpen Report</h1>
-            <h2>Created by Nick Hanson - Built with React</h2>
-            <h3>Version 0.1.0 - Using the Bullpen API</h3>
-            
-            <p class="underConstruction">🚧 This site is under construction - content coming soon 🚧</p>
-        </div>
-    );
+export default function App() {
+  return (
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<h1>Welcome to The Bullpen Report</h1>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="*" element={<h2>404: Page Not Found</h2>} />
+        </Routes>
+    </Router>
+  );
 }
-
-export default App;
