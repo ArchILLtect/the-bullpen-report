bc4e936912576b9b05e310021160bd718579de1f
ArchILLtect <nickhansonsr@gmail.com>
2025-05-17 13:49:02 -0500

    add: Navbar component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+/*
+Created by Nick Hanson
+Last modified: 5/17/25
+
+This file is part of The Bullpen Report project.
+The Bullpen Report is a baseball statistics viewer powered by AWS services.
+It connects to a custom API built using Lambda, API Gateway, and DynamoDB,
+delivering live team, player, and game data directly to the browser.
+The Bullpen Report is licensed under the MIT License.
+See the LICENSE file in the project root for more information.
+*/
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/stats">Stats</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
